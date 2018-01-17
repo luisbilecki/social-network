aab956093ca3fa3e9afd53cd3e42ff857752ab12
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-17 14:03:25 -0200

    Followers model to postgresql database

--- db/migrate/20171217172644_create_followers.rb
+++ db/migrate/20171217172644_create_followers.rb
@@ -8,7 +8,9 @@ class CreateFollowers < ActiveRecord::Migration[5.1]
 
       # Rails 5+ only: add foreign keys
       add_foreign_key :current, :users, column: :current_id, primary_key: :id
+      add_index :current, :current_id
       add_foreign_key :follower, :users, column: :follower_id, primary_key: :id
+      add_index :follower, :follower_id
     end
   end
 end
