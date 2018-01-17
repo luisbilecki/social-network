06a930592a9ed9c39d9df379d4582e8312482760
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-17 11:15:59 -0200

    Fix following model

--- db/migrate/20171217172644_create_followers.rb
+++ db/migrate/20171217172644_create_followers.rb
@@ -7,8 +7,8 @@ class CreateFollowers < ActiveRecord::Migration[5.1]
       t.timestamps
 
       # Rails 5+ only: add foreign keys
-      add_foreign_key :private_messages, :users, column: :current_id, primary_key: :id
-      add_foreign_key :private_messages, :users, column: :follower_id, primary_key: :id
+      add_foreign_key :current, :users, column: :current_id, primary_key: :id
+      add_foreign_key :follower, :users, column: :follower_id, primary_key: :id
     end
   end
 end
